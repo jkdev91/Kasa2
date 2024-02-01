58f13b154e01aff9f525724c757a3f674fbc1d60
dev91junior <devjk91@outlook.com>
2024-02-01 17:15:42 +0100

    create struture app

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+
+
+const Home = () => {
+    return (
+      <div className="homebody">
+        <h2>homepage</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+            Illum quo recusandae possimus porro unde architecto expedita dolores
+            ipsum! Dolor iusto quos quam voluptatum error dolore, ratione incidunt dignissimos earum omnis.
+        </p>
+        
+      </div>
+    );
+  };
+  
+  export default Home;
